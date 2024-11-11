9e403c28d37668c670303cf8a8fd1e85871d819d
Henrik L. Jensen <hnrk@eaaa.dk>
2024-11-11 10:13:11 +0100

    New adress in load-test

--- Minikube-Env/load-test.sh
+++ Minikube-Env/load-test.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-WeatherforecastURL=http://127.0.0.1:60863
+WeatherforecastURL=http://127.0.0.1:63754
 
 for i in {1..100}
 do 
     echo -n "${i} - "
-    curl $WeatherforecastURL/weatherforecast
-    curl $WeatherforecastURL/telemetry
-    curl $WeatherforecastURL/version
+    curl $WeatherforecastURL/weatherforecast > /dev/null
+    curl $WeatherforecastURL/telemetry > /dev/null
+    curl $WeatherforecastURL/version > /dev/null
     echo ""
 done
